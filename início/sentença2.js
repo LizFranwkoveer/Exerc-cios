ca334b64dddd9ef6ac512795abe96e13f2568f06
LizFranwkoveer <liz.santos6410@gmail.com>
2023-01-25 08:51:03 -0300

    first commit

---
+++ início/sentença2.js
@@ -0,0 +1,44 @@
+/* 
+  O código é organizado de um programa normalmente é organizado em pastas 
+  Além das pasa são organizados em arquivos de determinados tipos de extensão dependedo do tipo de programa 
+  se for c final ".c", se python final ".py", 
+  se c++ final ".cpp" e se java script final ".js";
+*/
+
+/*
+ Dentro dos Arquivos terão "sentenças" de códigos junto com os blocos;
+
+ --> observação: Deve-se definir todo o fluxo da sua sentença 
+ e de que ordem as sentenças serão executadas em blocos para melhor controle na execução das aplicações;
+*/
+
+
+
+
+
+
+// exemplos básicos
+{
+    console.log('Passo #01');
+    console.log('Passo #02');
+    console.log('Passo #03');
+}
+
+{
+    console.log('Passo #04');
+    console.log('Passo #05');
+}
+
+{
+ {
+    {
+        {
+            {
+                ;
+                ;
+                ;
+            }
+        }
+    }
+ }
+}
