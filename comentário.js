fe585e917a8656a84b794b7941a11573de1ac649
LizFranwkoveer <121194649+LizFranwkoveer@users.noreply.github.com>
2023-02-02 08:53:11 -0300

    Add files via upload

---
+++ comentário.js
@@ -0,0 +1,14 @@
+// comentário de uma única linha
+console.log('Linha 1')
+
+/*
+comentário de
+múltiplas linha
+*/
+console.log('Linha 2')
+
+/*
+*comentário de
+* multiplas linhas
+*/
+console.log('Linha 3')
---
+++ organizacao.js
@@ -0,0 +1,11 @@
+console.log("Sentença de código") // string é um texto literal associado a uma etrutura de controle e uma reetição 
+
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!')// Padrão do curso
+    }
+}
+
+// O Bloco agrupam sentenças de códigos e blocos de código para executar algo determinado
