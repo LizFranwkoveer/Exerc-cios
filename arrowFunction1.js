9729f575fa4c3225b1815f9503062b6cf257b5a2
LizFranwkoveer <121194649+LizFranwkoveer@users.noreply.github.com>
2023-02-23 19:27:28 -0300

    Add files via upload

---
+++ arrowFunction1.js
@@ -0,0 +1,18 @@
+let dobra = function (a) {
+    return 2 * a 
+}
+
+dobra = (a) => {
+    return 2 * a 
+}
+
+dobra = (a) => 2 * a // return implícito
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá' // possui um parametro
+console.log(ola())
