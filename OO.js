88bac441f36b1be2010ca1eeb7b78cf8ab193f45
LizFranwkoveer <121194649+LizFranwkoveer@users.noreply.github.com>
2023-03-01 21:39:10 -0300

    Add files via upload

---
+++ OO.js
@@ -0,0 +1,22 @@
+//CÓDIGO NÃO EXECUTÁVEL!!!
+
+//Procedural
+Processamento (valor1, valor2, valor3) 
+
+// 00
+objetos = {
+    valor1,
+    valor2,
+    valor3,
+    Processamento() {
+        // ...
+    }
+}
+
+objeto.Processamento() // foco passou a ser o objeto
+
+// Príncipios importantes:
+//1. abstração
+//2. encapusalamento
+//3. herança (prototype
+//4. polimorfismo
